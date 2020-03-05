b6c6ccb16e9fae4385c810e9825c25d107f6f5f5
Adam Szentesi <adam.szentesi@gmail.com>
2020-03-06 00:00:32 +0100

    Items 2 needs fixing

--- Assets/Scripts/PrefabManager.cs
+++ Assets/Scripts/PrefabManager.cs
@@ -1,14 +1,50 @@
-﻿using UnityEngine;
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
 
 namespace Librarian
 {
     public class PrefabManager
     {
+        private static Dictionary<Type, int> _PrefabIndices = new Dictionary<Type, int>();
+        private static List<GameObject> _Prefabs = new List<GameObject>();
+
+        public static int RegisterPrefab(Type type, GameObject prefab)
+        {
+            int ID;
+
+            if (_PrefabIndices.ContainsKey(type))
+            {
+                Debug.Log("OLD");
+                ID = _PrefabIndices[type];
+            }
+            else
+            {
+                Debug.Log("NEW");
+                ID = _Prefabs.Count;
+                _PrefabIndices.Add(type, ID);
+
+                GameObject copy = GameObject.Instantiate(prefab);
+                copy.SetActive(false);
+                _Prefabs.Add(copy);
+            }
+            
+            return ID;
+        }
+
+        public static GameObject Instantiate(int ID)
+        {
+            return GameObject.Instantiate(_Prefabs[ID]);
+        }
+
         // TODO: we could cache prefabs, might be faster, ore use assetbundles
         public static GameObject CreateInstance(string path)
         {
             Debug.Log("PATH " + path);
             return GameObject.Instantiate(Resources.Load<GameObject>(path));
         }
+
+
+
     }
 }
---
+++ Assets/Scripts/Test/ItemBody.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+
+public class ItemBody : MonoBehaviour
+{
+    public Sprite Icon;
+    //public string Name;
+
+    private ItemSoul _ItemSoul;
+    private bool _IsInitialized = false;
+
+    public void Init(ItemSoul itemSoul)
+    {
+        _ItemSoul = itemSoul;
+        _IsInitialized = true;
+    }
+
+    private void Start()
+    {
+        if (!_IsInitialized)
+        {
+            int ID = Librarian.PrefabManager.RegisterPrefab(GetType(), gameObject);
+            Debug.Log("ID " + gameObject + " " + ID);
+            _ItemSoul = new ItemSoul(ID, gameObject.name, Icon);
+        }
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyUp(KeyCode.C))
+        {
+            if (_ItemSoul != null)
+            {
+                _ItemSoul.Spawn(new Vector3());
+            }
+        }
+    }
+
+}
